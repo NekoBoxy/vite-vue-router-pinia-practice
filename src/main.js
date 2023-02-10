eaa824c9b49ec2dcb71b7c45c08a4e998636e43e
CatBoxy <catboxy@gmail.com>
2023-02-10 21:36:01 +0800

    npm axios

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
-import { createApp } from "vue";
+import { createApp } from "vue"; // 來自套件
 import { createPinia } from "pinia";
 import axios from "axios";
 import VueAxios from "vue-axios";
 
-import App from "./App.vue";
+import App from "./App.vue"; // 來自src
 import router from "./router";
 
 import "./assets/main.css";
 
 const app = createApp(App);
 
-app.use(createPinia());
+app.use(createPinia()); // 使用套件
 app.use(router);
 app.use(VueAxios, axios);
 
