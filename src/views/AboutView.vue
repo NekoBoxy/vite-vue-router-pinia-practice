8fcd5e1b9cb4267e3d596b16c463e5287d52d24e
CatBoxy <catboxy@gmail.com>
2023-02-10 17:05:22 +0800

    加入axios、router 改hash mode、.vue串了api

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,29 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    {{ data }}
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      data: {},
+    };
+  },
+  mounted() {
+    this.$http.get("https://randomuser.me/api/").then((response) => {
+      // console.log(response.data);
+      this.data = response.data.results[0];
+      console.log(this.data);
+    });
+    // .catch(() => {
+    //   console.log();
+    // });
+  },
+};
+</script>
 
 <style>
 @media (min-width: 1024px) {
